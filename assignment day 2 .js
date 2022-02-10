6edc9223431f3b9502733b02cb7cb756167b35e8
Ankita kumari <97899824+A-nkitA1Kumari@users.noreply.github.com>
2022-02-11 00:35:40 +0530

    Create assignment day 2 .js

---
+++ assignment day 2 .js
@@ -0,0 +1,79 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+    // ARITHMETIC (+,-,/,%,*)
+    var a = 10
+    var b = 20
+    var res = a+b
+    var res1 = a-b
+    var res2 = a/b
+    var res3 = a*b
+    var res4= a%b
+    console.log(res);
+    console.log(res1)
+    console.log(res2)
+    console.log(res3)
+    console.log(res4)
+    
+   
+// assignment operators(=, +=, -=, /= %=)
+    var res = 30
+    var res1 = a += 40
+    var res2 = a -= 4
+    var res3 = b /= 2
+    var res4 = b %= 3
+    console.log(res);
+    console.log(res1)
+    console.log(res2)
+    console.log(res3)
+    console.log(res4)
+
+    // comparison operators(==,!=,>,<,>=,<=)
+    var res= a==b
+    var res1 = a!=b 
+    var res2 = a>b 
+    var res3 = b<a 
+    var res4 = b>=a 
+    var res5 = a<=b
+    console.log(res);
+    console.log(res1)
+    console.log(res2)
+    console.log(res3)
+    console.log(res4)
+    console.log(res5)
+
+    //identity operators
+    var res=  a===b
+    var res1= a!==b
+    console.log(res)
+    console.log(res1)
+
+    // conditional operators
+    var res= (a<=b && b>a)
+    var res1 = (a>=b || b<=a)
+    var res2 = (a>b && b>a)
+    var res3 = (a<=b || b!=a)
+    console.log(res)
+    console.log(res1)
+    console.log(res2)
+    console.log(res3)
+    
+    // unary opeators
+    
+    var res= a++
+    var res1= b--
+    console.log(a)
+    console.log(b)
+
+
+</script>
+</html>
